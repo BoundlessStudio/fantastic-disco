84673e9d6522ed2d0b7b2bdcfa06686693ccfc11
RGBKnights <master@rgbknights.com>
2025-11-14 13:26:49 -0500

    added parallel

--- app/lib/agents/core-agent.ts
+++ app/lib/agents/core-agent.ts
@@ -1,10 +1,11 @@
 import {
-  tool,
   stepCountIs,
   Experimental_Agent as Agent,
   type Experimental_InferAgentUIMessage as InferAgentUIMessage,
+  type Tool
 } from 'ai';
-import { z } from 'zod';
+// import { z } from 'zod';
+import { searchTool, extractTool } from '@parallel-web/ai-sdk-tools';
 import { createMem0 } from "@mem0/vercel-ai-provider";
 
 const { openaiApiKey, mem0ApiKey } = useRuntimeConfig();
@@ -19,37 +20,10 @@ const mem0 = createMem0({
 
 export const CoreAgent = new Agent({
    model: mem0("gpt-5.1", { user_id: "113600246600420663190" }),
-    stopWhen: stepCountIs(10),
+    stopWhen: stepCountIs(100),
     tools: {
-      weather: tool({
-        description: 'Get the weather in a location (fahrenheit)',
-        inputSchema: z.object({
-          location: z
-            .string()
-            .describe('The location to get the weather for'),
-        }),
-        execute: async ({ location }) => {
-          const temperature = Math.round(Math.random() * (90 - 32) + 32);
-          return {
-            location,
-            temperature,
-          };
-        },
-      }),
-      convert_fahrenheit_to_celsius: tool({
-        description: 'Convert a temperature in fahrenheit to celsius',
-        inputSchema: z.object({
-          temperature: z
-            .number()
-            .describe('The temperature in fahrenheit to convert'),
-        }),
-        execute: async ({ temperature }) => {
-          const celsius = Math.round((temperature - 32) * (5 / 9));
-          return {
-            celsius,
-          };
-        },
-      }),
+      'web-search': searchTool as Tool,
+      'extract': extractTool as Tool,
     },
 });
 
